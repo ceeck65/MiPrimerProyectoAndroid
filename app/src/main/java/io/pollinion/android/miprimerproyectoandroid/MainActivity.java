c406759cc44bc3115ac2cf99344400d9f5e27975
Mario Fernandez <alespcs@gmail.com>
2016-06-28 17:53:04 -0430

    Agregado Nuevo Activity

--- app/src/main/java/io/pollinion/android/miprimerproyectoandroid/MainActivity.java
+++ app/src/main/java/io/pollinion/android/miprimerproyectoandroid/MainActivity.java
@@ -1,13 +1,34 @@
 package io.pollinion.android.miprimerproyectoandroid;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEnter;
+        btnEnter = (Button) findViewById(R.id.btnEnter);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
+
 }
